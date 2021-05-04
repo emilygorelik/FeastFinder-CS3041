80e670a045f0df793ecc3c5c7c15c48258886863
Emily Gorelik <emilygorelik2000@gmail.com>
2021-05-04 19:14:40 -0400

    make survey pages

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var active_group = "";
+
+function getActiveGroup(inGroup){
+    console.log("dvsdvsdvs");
+    active_group = inGroup;   
+}
+function setActiveGroupName(){
+    document.getElementById("group_name_cell").innerHTML = active_group;
+}
